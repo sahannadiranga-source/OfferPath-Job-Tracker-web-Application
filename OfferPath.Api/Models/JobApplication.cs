ef24f78a1caf4618333737d247d25b1c307e61d3
nadiranga31 <133639227+nadiranga31@users.noreply.github.com>
2026-03-12 21:01:22 +0530

    API and ef migration

---
+++ OfferPath.Api/Models/JobApplication.cs
@@ -0,0 +1,15 @@
+﻿namespace OfferPath.Api.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string Company { get; set; } = "";
+        public string Position { get; set; } = "";
+        public string Location { get; set; } = "";
+        public string Status { get; set; } = "";
+        public DateTime AppliedDate { get; set; }
+        public string Notes { get; set; } = "";
+    }
+}
--- OfferPath.Client/obj/Debug/net8.0/OfferPath.Client.AssemblyInfo.cs
+++ OfferPath.Client/obj/Debug/net8.0/OfferPath.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OfferPath.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23872aeb5a276348ed9a06d07cd148d0cb23071")]
 [assembly: System.Reflection.AssemblyProductAttribute("OfferPath.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OfferPath.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
